dc6636c8f84b573292c3ea4e0119b0a7c8c8291d
Junaid Farooq <junaid@evercam.io>
2016-10-30 21:21:30 +0500

    Added tweetController resource route

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Pitter.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/tweets", TweetController
   end
 
   # Other scopes may use custom stacks.
